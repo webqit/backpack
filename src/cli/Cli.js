c03a99e8525cc10ff8c50176712f774b2007a01b
Oxford <ox_harris@yahoo.com>
2022-10-19 05:03:41 +0100

    Fix: this.merge() undefined in Cli

--- src/cli/Cli.js
+++ src/cli/Cli.js
@@ -133,23 +133,31 @@ export default class Cli {
         cx.logger.log('');
     }
 
-    merge(a, b, arrFn = 0, keys = []) {
-        let result = arguments.length > 3 ? [] : {};
-        (arguments.length > 3 ? keys : Object.keys(a).concat(Object.keys(b))).forEach(k => {
-            if (_isObject(a[k]) && _isObject(b[k])) {
-                result[k] = this.merge(a[k], b[k], arrFn);
-            } else if (_isArray(a[k]) && _isArray(b[k]) && arrFn) {
-                if (arrFn === 'merge') {
-                    result[k] = a[k].concat(b[k]);
-                } else if (arrFn === 'patch') {
-                    let longer = a[k].length > b[k].length ? a[k] : b[k];
-                    result[k] = this.merge(a[k], b[k], arrFn, Object.keys(longer).map(k => parseInt(k)));
-                }
-            } else {
-                result[k] = k in b ? b[k] : a[k];
-            }
-        });
-        return result;
+    merge(a, b, arrFn = null) {
+        return merge(...arguments);
+    }
+    
+    static merge(a, b, arrFn = null) {
+        return merge(...arguments);
     }
 
+}
+
+function merge(a, b, arrFn = 0, keys = []) {
+    let result = arguments.length > 3 ? [] : {};
+    (arguments.length > 3 ? keys : Object.keys(a).concat(Object.keys(b))).forEach(k => {
+        if (_isObject(a[k]) && _isObject(b[k])) {
+            result[k] = merge(a[k], b[k], arrFn);
+        } else if (_isArray(a[k]) && _isArray(b[k]) && arrFn) {
+            if (arrFn === 'merge') {
+                result[k] = a[k].concat(b[k]);
+            } else if (arrFn === 'patch') {
+                let longer = a[k].length > b[k].length ? a[k] : b[k];
+                result[k] = merge(a[k], b[k], arrFn, Object.keys(longer).map(k => parseInt(k)));
+            }
+        } else {
+            result[k] = k in b ? b[k] : a[k];
+        }
+    });
+    return result;
 }
